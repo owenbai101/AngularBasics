5140306d734da0ae621ee535bad712c972b96aa7
owen 2 <owenbai101@gmail.com>
2022-12-15 17:32:39 +0800

    2022121501

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularBasics';
+  name ='owen';
+  imgurl ='https://picsum.photos/id/237/500/500'
+  getName()
+  {
+    return this.name; 
+  }
+
+  changeImage(e: KeyboardEvent){
+    this.imgurl =(e.target as HTMLInputElement).value;
+  }
+
+  logImg(event: string){
+    console.log(event)
+  }
 }
